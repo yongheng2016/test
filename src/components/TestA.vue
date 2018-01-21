1c7e5dbda123ef3212d932be5926803c9a800a44
yongheng <1074827582@qq.com>
2018-01-21 22:47:23 +0800

    父子组件数据传递

---
+++ src/components/TestA.vue
@@ -0,0 +1,31 @@
+<template>
+ <div id="app">
+ <child1
+ :p-child1="child1"
+ :p-child2="child2"
+ v-on:test1="onTest1"
+ v-on:test2="onTest2"> 
+ </child1>
+ </div>
+</template>
+<script>
+import Child1 from "./TestB.vue";
+export default {
+  data() {
+    return {
+        child1: '11111111',
+        child2: '22222222'
+        
+    };
+  },
+  components: { Child1 },
+  methods: {
+    onTest1() {
+      console.log("test1 running...");
+    },
+    onTest2() {
+      console.log("test2 running");
+    }
+  }
+};
+</script>
---
+++ src/components/TestB.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="child-1">
+ <p>in child1:</p>
+ <p>props: {{pChild1}}</p>
+ <p>$attrs: {{$attrs}}</p>
+ <hr>
+ <!-- C组件中能直接触发test的原因在于 B组件调用C组件时 使用 v-on 绑定了$listeners 属性 -->
+ <!-- 通过v-bind 绑定$attrs属性，C组件可以直接获取到A组件中传递下来的props（除了B组件中props声明的） -->
+ <child2 v-bind="$attrs" v-on="$listeners"></child2>
+ </div>
+</template>
+<script>
+import Child2 from "./TestC.vue";
+export default {
+  props: ["pChild1"],
+  data() {
+    return {};
+  },
+  inheritAttrs: false,
+  components: { Child2 },
+  mounted() {
+    this.$emit("test1");
+  }
+};
+</script>
