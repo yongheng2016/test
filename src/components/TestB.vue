44a81c61fb377edceb61619510ff0281b8ff6a20
yongheng <1074827582@qq.com>
2018-03-19 17:35:36 +0800

    Update TestB.vue

--- src/components/TestB.vue
+++ src/components/TestB.vue
@@ -4,9 +4,9 @@
  <p>props: {{pChild1}}</p>
  <p>$attrs: {{$attrs}}</p>
  <hr>
- <!-- C组件中能直接触发test的原因在于 B组件调用C组件时 使用 v-on 绑定了$listeners 属性 -->
- <!-- 通过v-bind 绑定$attrs属性，C组件可以直接获取到A组件中传递下来的props（除了B组件中props声明的） -->
  <child2 v-bind="$attrs" v-on="$listeners"></child2>
+  <!-- C组件中能直接触发test的原因在于 B组件调用C组件时 使用 v-on 绑定了$listeners 属性 -->
+ <!-- 通过v-bind 绑定$attrs属性，C组件可以直接获取到A组件中传递下来的props（除了B组件中props声明的） -->
  </div>
 </template>
 <script>
