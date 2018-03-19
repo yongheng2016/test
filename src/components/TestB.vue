f42b9a4d6dfc42cc861e0cd345108c2224636f49
yongheng <1074827582@qq.com>
2018-03-19 17:39:05 +0800

    Update TestB.vue

--- src/components/TestB.vue
+++ src/components/TestB.vue
@@ -5,6 +5,7 @@
  <p>$attrs: {{$attrs}}</p>
  <hr>
  <child2 v-bind="$attrs" v-on="$listeners"></child2>
+    <!-- B组件在中间起到了承上启下的作用（链接 A | C 组件） -->
     <!-- A组件中能直接监听C组件提交的事件的原因在于 B组件调用C组件时 使用 v-on 绑定了$listeners 属性 -->
     <!-- 通过v-bind 绑定$attrs属性，C组件可以直接获取到A组件中传递下来的props（除了B组件中props声明的） -->
  </div>
